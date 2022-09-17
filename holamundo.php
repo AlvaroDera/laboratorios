2a8f24c072b0d1839fabeb036c35de029edf9da4
AlvaroDera <alvaro.alvarez@utp.ac.pa>
2022-09-16 20:16:46 -0500

    probando

---
+++ holamundo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <title> Ejemplo Basico PHP </title>
+    </head>
+
+    <body>
+        <?php echo 'Hola mundo'; ?>
+    </body>
+</html>
