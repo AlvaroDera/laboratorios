17daa131dfdd28d867447484cf5c6807d75669eb
AlvaroDera <alvaro.alvarez@utp.ac.pa>
2022-10-14 23:15:15 -0500

    Agregando Lab14

---
+++ lab14/login.php
@@ -0,0 +1,80 @@
+<?php
+    session_start();
+
+    if (isset($_REQUEST['usuario']) && isset($_REQUEST['clave'])) {
+        $usuario = $_REQUEST['usuario'];
+        $clave = $_REQUEST['clave'];
+
+        $salt = substr ($usuario, 0, 2);
+        $clave_crypt = crypt ($clave, $salt);
+
+        require_once("class/usuarios.php");
+
+        $obj_usuarios = new usuarios();
+        $usuario_validado = $obj_usuarios->validar_usuario($usuario,$clave_crypt);
+
+        foreach ($usuario_valido as $array_resp) {
+            foreach ($array_resp as $value) {
+                $nfilas=$value;
+            }
+        }
+
+        if ($nfilas > 0) {
+            $usuario_valido = $usuario;
+            $_SESSION["usuario_valido"] = $usuario_valido;
+        }
+    }
+?>
+<!DOCTYPE html public "-//W3C/DTD HTML 4.0//EN"
+    "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <title> Laboratorio 14 - Login al sitio de Noticias </title>
+</head>
+<body>
+    <?php
+        //Sesion Iniciada
+        if (isset($_SESSION["usuario_valido"])) {
+            ?>
+            <h1> Gestion de Noticias </h1>
+            <hr>
+
+            <ul>
+                <li><a href="lab142.php"> Listar todas las noticias </a>
+                <li><a href="lab143.php"> Listar noticias por partes </a>
+                <li><a href="lab144.php"> Buscar noticias </a>
+            </ul>
+
+            <hr>
+            <p>[ <a href='logout.php'> Desconectar </a> ] </p>
+    <?php
+        }
+    // Intento de entrada fallido
+    else if (isset($usuario)) {
+        print ("<br><br>\n");
+        print ("<p align='center'> Acceso no autorizado </p>\n");
+        print ("<p align='center'>[ <a href='login.php'> Conectar </p>\n");
+    }
+    // Sesion no iniciada
+    else {
+        print ("<br><br>\n");
+        print ("<p class='parrafocentrado'> Esta zona tiene el acceso restringido.<br>" .
+        " Para entrar debe identificarse </p>\n");
+
+        print ("<form class='entrada' name='login' action='login.php' method='POST'>\n");
+        print ("<p><label class='etiqueta-entrada'> Usuario:</label>\n");
+        print ("    <input type='text' name='usuario' size='15'></p>\n");
+        print ("<p><label class='etiqueta-entrada'> Clave:</label>\n");
+        print ("    <input type='password' name='clave' size='15'></p>\n");
+        print ("<p><input type='submit' value='entrar'></label>\n");
+        print ("    </form>\n");
+
+        print ("<p class='parrafocentrado'>Nota: si no dispone de identificacion o tiene problemas " .
+        "para entrar<br>pongase en contacto en el " .
+        "<a href='MAILTO:webmaster@localhost'> Administrador </a> del sitio</p>\n");
+        }
+    ?>
+</body>
+</html>
